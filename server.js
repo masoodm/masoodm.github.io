819afd3940efc2108740a277854f4ef2b401c0b7
Masood Mawlavizada <masmawlav@ucdavis.edu>
2017-07-06 16:56:49 -0700

    routing check

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require ('express');
 var app = express();
 
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname));
 app.use('/bower_components', express.static(__dirname+'/bower_components'));
 app.use('/node_modules', express.static(__dirname+'/node_modules'));
 
