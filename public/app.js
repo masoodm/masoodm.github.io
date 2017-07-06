1bbf92bbfd62f19b94f121a1b9dc07bade0de6b1
Masood Mawlavizada <masmawlav@ucdavis.edu>
2017-07-06 13:10:27 -0700

    check if sererver.js will work on github

---
+++ public/app.js
@@ -0,0 +1,73 @@
+(function(){
+	var app = angular.module('mainModule', ['ngMaterial']);
+
+	app.run(function($rootScope){
+		$rootScope.tab = 1;
+		$rootScope.isSelected = function(num){
+			return $rootScope.tab === num;
+		}
+		$rootScope.changeTabValue = function(num){
+			$rootScope.tab = num;
+		};
+	});
+
+	// app.config(function($routeProvider) {
+	// 	$routeProvider
+	// 	.when("/", {
+	// 		templateURL: "index.html"
+	// 	})
+	// 	.when("/photography", {
+	// 		templateURL: "photography.html"
+	// 	})
+	// 	.when("/projects", {
+	// 		templateURL: "projects.html"
+	// 	})
+	// 	.when("/about", {
+	// 		templateURL: "about.html"
+	// 	});
+	// });
+
+	app.controller('HeaderController', function($scope, $rootScope){
+
+
+	});
+
+	app.controller('BodyController', function($scope, $rootScope){
+		$scope.photography = [
+		{
+			type: "Senior Portraits",
+			description: "Will replace with images",
+			images: [
+				"individual/DSC_002.jpg", 
+				"individual/DSC_0049.jpg", 
+				"individual/DSC_0113.jpg", 
+				"individual/DSC_0164.jpg", 
+				"individual/DSC_0299.jpg", 
+				"individual/DSC_0383.jpg",
+				"individual/DSC_0986.jpg", 
+				"individual/DSC_0991.jpg",
+				"individual/File_000.jpeg"
+			]
+		},
+		{
+			type: "Baby Portraits",
+			description: "Will replace soon"
+		},
+		{
+			type: "Events",
+			description: "Will replace soon"
+		},
+		{
+			type: "Group Photos",
+			description: "Will relace soon",
+			images: [
+				"group/DSC_0980.jpg",
+				"group/DSC_0436.jpg",
+				"group/DSC_0009.jpg", 
+				"group/DSC_0011.jpg"			
+			]
+		}
+		];
+		
+	});
+})();
---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require ('express');
+var app = express();
+
+app.use(express.static(__dirname + "/public"));
+app.use('/bower_components', express.static(__dirname+'/bower_components'));
+app.use('/node_modules', express.static(__dirname+'/node_modules'));
+
+
+
+var server = app.listen(process.env.PORT || 8080, function () {
+		var port = server.address().port;
+		console.log("App now running on port", port);
+	});
